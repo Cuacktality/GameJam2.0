01586c1e480c74798a7390484fa5b32dcd30342f
Daniel Contreras <danielorlando520@gmail.com>
2021-06-07 23:45:51 -0500

    NewCanvas

--- Assets/scrips/Player.cs
+++ Assets/scrips/Player.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private Rigidbody2D myRig;
     public float speed;
     public float jump;
@@ -15,101 +15,76 @@ public class Player : MonoBehaviour
     private bool resetearSalto;
 
     public string scorestring = "Score: ";
-    public Text textScore;
-    private int score ;
-    
-    void Start()
-    {
-        myRig = GetComponent<Rigidbody2D>();
-        flip = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
+    [SerializeField] TMP_Text textScore;
+    private int score;
+
+    void Start () {
+        myRig = GetComponent<Rigidbody2D> ();
+        flip = GetComponent<SpriteRenderer> ();
+        anim = GetComponent<Animator> ();
     }
 
-   
-    void Update()
-    {
-        myRig.velocity = new Vector2(horizontal * speed, myRig.velocity.y);
-        horizontal = Input.GetAxis("Horizontal");
-        Mover();
-        Saltar();
-        Scoretext();
-        
-        
+    void Update () {
+        myRig.velocity = new Vector2 (horizontal * speed, myRig.velocity.y);
+        horizontal = Input.GetAxis ("Horizontal");
+        Mover ();
+        Saltar ();
+        Scoretext ();
+
     }
-    void Scoretext()
-    {
-        if (textScore != null)
-        {
-            textScore.text = scorestring + score.ToString();
+    void Scoretext () {
+        if (textScore != null) {
+            textScore.text = scorestring + score.ToString ();
         }
-        
+
     }
-    private void Mover()
-    {
-        
-        if (horizontal != 0)
-        {
-            anim.SetBool("Mover", true);
+    private void Mover () {
+
+        if (horizontal != 0) {
+            anim.SetBool ("Mover", true);
+        } else {
+            anim.SetBool ("Mover", false);
         }
-        else
-        {
-            anim.SetBool("Mover", false);
-        }
-        if (horizontal > 0)
-        {
+        if (horizontal > 0) {
             flip.flipX = false;
-            
+
         }
-        if (horizontal < 0)
-        {
+        if (horizontal < 0) {
             flip.flipX = true;
-            
+
         }
-        
-    }
-    void Saltar()
-    {
-        if (Input.GetButtonDown("Jump") && saltar == true)
-        {
-            myRig.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            anim.SetBool("Saltar", true);
-        }
-        else
-        {
-            anim.SetBool("Saltar", false);
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-    if (other.gameObject.tag == "Piso")
-    {
-        saltar = true;
-    }
 
     }
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Piso")
-        {
+    void Saltar () {
+        if (Input.GetButtonDown ("Jump") && saltar == true) {
+            myRig.AddForce (Vector2.up * jump, ForceMode2D.Impulse);
+            anim.SetBool ("Saltar", true);
+        } else {
+            anim.SetBool ("Saltar", false);
+        }
+    }
+    private void OnCollisionEnter2D (Collision2D other) {
+        if (other.gameObject.tag == "Piso") {
+            saltar = true;
+        }
+
+    }
+    private void OnCollisionExit2D (Collision2D other) {
+        if (other.gameObject.tag == "Piso") {
             saltar = false;
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            speed=0;
+    private void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.gameObject.tag == "Enemy") {
+            speed = 0;
             jump = 0;
-            anim.SetTrigger("Morir");
-            Destroy(this.gameObject,1.4f);
-            
+            anim.SetTrigger ("Morir");
+            Destroy (this.gameObject, 1.4f);
+
         }
-        if (collision.gameObject.tag == "Items")
-        {
+        if (collision.gameObject.tag == "Items") {
             score++;
         }
     }
-   
-
 
 }
--- Assets/scrips/StartMenu.cs
+++ Assets/scrips/StartMenu.cs
@@ -11,21 +11,27 @@ public class StartMenu : MonoBehaviour {
     [SerializeField] Button play;
     [SerializeField] Image fader;
     [SerializeField] Animator fdAnim;
-
+    [SerializeField] TMP_Text upT;
+    [SerializeField] TMP_Text lwT;
     [Header ("Cinematic animation")]
     [SerializeField] GameObject img;
 
+    private Color tP = new Color32 (255, 255, 255, 0);
+    private Color wT = new Color32 (255, 255, 255, 190);
+
     // Start is called before the first frame update
     void Start () {
+        upT.gameObject.GetComponent<TextMeshProUGUI> ().color = tP;
+        lwT.gameObject.GetComponent<TextMeshProUGUI> ().color = tP;
         StartCoroutine (Welcome ());
     }
 
     IEnumerator Welcome () {
-        Debug.Log ("Desvaneciendo imagen");
         fdAnim.SetBool ("Fade", false);
         yield return new WaitUntil (() => fader.color.a == 0);
         fader.gameObject.SetActive (false);
-        Debug.Log ("Imagen desvanecida, imagen desactivada");
+        upT.gameObject.GetComponent<TextMeshProUGUI> ().color = Color.Lerp (tP, wT, 1f);
+        lwT.gameObject.GetComponent<TextMeshProUGUI> ().color = Color.Lerp (tP, wT, 3f);
     }
 
     public void BStartGame () {
