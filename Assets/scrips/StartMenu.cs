1932d68313d30ea830fd98d2958f4090796aef93
Daniel Contreras <danielorlando520@gmail.com>
2021-06-08 03:07:12 -0500

    History

--- Assets/scrips/StartMenu.cs
+++ Assets/scrips/StartMenu.cs
@@ -8,22 +8,32 @@ using UnityEngine.UI;
 public class StartMenu : MonoBehaviour {
 
     [Header ("Main Menu Items")]
+    [SerializeField] GameObject mainPanel;
     [SerializeField] Button play;
+    [SerializeField] Button next;
     [SerializeField] Image fader;
     [SerializeField] Animator fdAnim;
     [SerializeField] TMP_Text upT;
     [SerializeField] TMP_Text lwT;
+
     [Header ("Cinematic animation")]
-    [SerializeField] GameObject img;
+    [SerializeField] GameObject[] img;
+    [SerializeField] int cN = 0;
 
     private Color tP = new Color32 (255, 255, 255, 0);
-    private Color wT = new Color32 (255, 255, 255, 190);
+    private Color wT = new Color32 (255, 255, 255, 255);
+    private Color bK = new Color32 (0, 0, 0, 255);
 
     // Start is called before the first frame update
     void Start () {
         upT.gameObject.GetComponent<TextMeshProUGUI> ().color = tP;
         lwT.gameObject.GetComponent<TextMeshProUGUI> ().color = tP;
+        next.gameObject.SetActive (false);
         StartCoroutine (Welcome ());
+        for (int i = 0; i <= img.Length - 1; i++) {
+            img[i].SetActive (false);
+            img[i].gameObject.GetComponent<Image> ().color = tP;
+        }
     }
 
     IEnumerator Welcome () {
@@ -38,6 +48,54 @@ public class StartMenu : MonoBehaviour {
         StartCoroutine (StartGame ());
     }
     IEnumerator StartGame () {
+        fader.gameObject.SetActive (true);
+        fdAnim.SetBool ("Fade", true);
+        yield return new WaitUntil (() => fader.color.a == 1);
+        fader.color = Color.Lerp (fader.color, tP, 4f);
+        fader.gameObject.SetActive (false);
+        mainPanel.SetActive (false);
+        img[0].SetActive (true);
+        img[0].gameObject.GetComponent<Image> ().color = Color.Lerp (tP, wT, 2f);
+        cN = 1;
+        next.gameObject.SetActive (true);
+    }
+
+    public void BNext () {
+        switch (cN) {
+            case 1:
+                {
+                    img[0].SetActive (false);
+                    img[1].SetActive (true);
+                    img[1].gameObject.GetComponent<Image> ().color = Color.Lerp (tP, wT, 2f);
+                    cN = 2;
+                    break;
+                }
+            case 2:
+                {
+                    img[1].SetActive (false);
+                    img[2].SetActive (true);
+                    img[2].gameObject.GetComponent<Image> ().color = Color.Lerp (tP, wT, 2f);
+                    cN = 3;
+                    break;
+                }
+            case 3:
+                {
+                    img[2].SetActive (false);
+                    img[3].SetActive (true);
+                    img[3].gameObject.GetComponent<Image> ().color = Color.Lerp (tP, wT, 2f);
+                    cN = 4;
+                    break;
+                }
+            case 4:
+                {
+                    StartCoroutine (LoadGame ());
+                    break;
+                }
+        }
+    }
+
+    IEnumerator LoadGame () {
+
         fader.gameObject.SetActive (true);
         fdAnim.SetBool ("Fade", true);
         yield return new WaitUntil (() => fader.color.a == 1);
